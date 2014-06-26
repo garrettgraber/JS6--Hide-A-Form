0a1442c1ec5ab51d0c81e6aafbbb18f1e215453d
garrettgraber <garrett_graber@yahoo.com>
2014-06-26 16:31:19 -0600

    JS6 All done, all parts, sort of including the bonus

--- main.js
+++ main.js
@@ -12,26 +12,54 @@ $(document).on('ready', function() {
       var bookData = $('#book-enter').val();
       var jsLibData = $('#js-libraries-enter').val();
 
-      alert('Name: ' + nameData);
-      alert('Bio: ' + bioData);
-      alert('Books: ' + bookData);
-      alert('JS Libraries: ' + jsLibData);
+      window.nameData = nameData;
+      console.log('Name: ' + nameData + ' type of: ' + typeof(nameData));
+      console.log('Bio: ' + bioData);
+      console.log('Books: ' + bookData);
+      console.log('JS Libraries: ' + jsLibData);
 
+      var nameDataObj = $('<p/>', {text: ' ' + nameData + ' ', class:'user-data'});
+      var bioDataObj = $('<p/>', {text: ' ' + bioData + ' ', class:'user-data'});
+      var bookDataObj = $('<p/>', {text: ' ' + bookData + ' ', class:'user-data'});
+      var jsLibDataObj = $('<p/>', {text: ' ' + jsLibData + ' ', class:'user-data'});
+      /*var nameDataObj = $('p').text(nameData);*/
+      nameDataObj.appendTo('#name-data');
+      bioDataObj.appendTo('#bio-data');
+      bookDataObj.appendTo('#book-data');
+      jsLibDataObj.appendTo('#js-libraries-data');
+      console.log(nameDataObj);
+      /*
       $('#name-data').text(nameData);
       $('#bio-data').text(bioData);
       $('#book-data').text(bookData);
       $('#js-libraries-data').text(jsLibData);
+      */
 
     });
 
-    $('.show-form-button').on('click', function() {
+    $('#show-form-button').on('click', function() {
       var formVisibilityStatus = $('#form-input-table').css('visibility');
+      var showFormButtonText = $('#show-form-button').text();
+      console.log(showFormButtonText);
       console.log(formVisibilityStatus);
       if (formVisibilityStatus === 'hidden') {
         $('#form-input-table').css('visibility','visible');
+         $('#show-form-button').text('Hide Forms');
       }
       else {
         $('#form-input-table').css('visibility','hidden');
+        $('#show-form-button').text('Show Forms');
       }
+    });
+
+    $(document).on('click', '.user-data', function(){
+      $(this).remove();
+
+    });
+
+    $('#clear-form').on('click', function() {
+      var formEntryPoint = $('.form-entry-point').val();
+      console.log(formEntryPoint);
+      $('.form-entry-point').val('');
     });
 });
